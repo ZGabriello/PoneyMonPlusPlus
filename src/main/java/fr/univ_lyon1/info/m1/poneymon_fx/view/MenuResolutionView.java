90c1cab83bd63c328b644c4c024ef8d6e5b83c04
Gabriello ZAFIFOMENDRAHA <gabriello.zafifomendraha@gmail.com>
2018-10-31 16:42:23 +0100

    resolution sonar duplicated error

--- src/main/java/fr/univ_lyon1/info/m1/poneymon_fx/view/MenuResolutionView.java
+++ src/main/java/fr/univ_lyon1/info/m1/poneymon_fx/view/MenuResolutionView.java
@@ -5,24 +5,17 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import javafx.animation.TranslateTransition;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.effect.GaussianBlur;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.util.Duration;
 
 /**
  * Vue du menu resolution.
@@ -31,25 +24,6 @@ import javafx.util.Duration;
 public class MenuResolutionView extends View {
 
     int idMainView;
-
-    static final Font FONT = Font.font("", FontWeight.BOLD, 50);
-
-    /**
-     * Couleurs récupérées de la crinière des poneys.
-     */
-    static final Color BLUE = Color.web("#3121B8");
-    static final Color LIGHTBLUE = Color.web("#5F40DA");
-    static final Color GREEN = Color.web("#2EC268");
-    static final Color LIGHTGREEN = Color.web("#4CE58A");
-    static final Color ORANGE = Color.web("#E62917");
-    static final Color LIGHTORANGE = Color.web("#F46A39");
-    static final Color PURPLE = Color.web("#A12BC8");
-    static final Color LIGHTPURPLE = Color.web("#CB44EC");
-    static final Color YELLOW = Color.web("#E47702");
-    static final Color LIGHTYELLOW = Color.web("#FCB31F");
-
-    Color[] titleColors
-            = new Color[]{LIGHTBLUE, LIGHTGREEN, LIGHTORANGE, LIGHTPURPLE, LIGHTYELLOW};
 
     Controller controller;
 
@@ -138,29 +112,6 @@ public class MenuResolutionView extends View {
                 new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
     }
 
-    private Node createTitle(String title) {
-        HBox letters = new HBox(0);
-        letters.setAlignment(Pos.CENTER);
-
-        for (int i = 0; i < title.length(); i++) {
-            Text letter = new Text(title.charAt(i) + "");
-            letter.setFont(FONT);
-            letter.setFill(titleColors[i % titleColors.length]);
-            letters.getChildren().add(letter);
-
-            TranslateTransition tt = new TranslateTransition(Duration.seconds(2), letter);
-            tt.setDelay(Duration.millis(i * 50));
-            tt.setToY(-25);
-            tt.setAutoReverse(true);
-            tt.setCycleCount(TranslateTransition.INDEFINITE);
-            tt.play();
-        }
-
-        letters.setEffect(new GaussianBlur(2));
-
-        return letters;
-    }
-
     private MenuItem getMenuItem(int index) {
         return menuItems.get(index);
     }
@@ -192,13 +143,5 @@ public class MenuResolutionView extends View {
         setPrefSize(newWidth, newHeight);
         controller.changeResolution(idMainView, newWidth, newHeight);
     }
-
-    public int getWidthNew() {
-        return (int) getPrefWidth();
-    }
-
-    public int getHeightNew() {
-        return (int) getPrefHeight();
-    }
-
+    
 }
